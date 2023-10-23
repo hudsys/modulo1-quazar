937d3acfde4fbd13314891e2ac1c5ae91916ca11
hudson <hudsys@gmail.com>
2023-10-23 19:36:39 -0300

    upload my first projetc with quazar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="flex justify-between items-center q-pa-md">
+        <q-icon name="keyboard_arrow_left" size="25px"></q-icon>
+        <q-toolbar-title class="text-center">GEAF-WEB</q-toolbar-title>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />        
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Menu Principal
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Início',
+    caption: 'Início da Aplicação',
+    icon: 'school',
+    link: 'http://www.geaf-web.com.br:9010/'
+  },
+  {
+    title: 'Cursos',
+    caption: 'Ir para Site de Cursos',
+    icon: 'code',
+    link: 'http://www.geaf-web.com.br:9020/'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  components: {
+    EssentialLink
+  },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus">
+  .header { 
+    background-color: #e067b8;
+  }
+  .q-page-container {
+    padding-top : 65px !important;
+  }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-page class="container flex column justify-around flex-center justify-around">
+  <q-img
+      src="../assets/Vetor-Logo-Cor.jpeg"
+      spinner-color="white"
+      style="height: auto; max-width: 230px"
+   />
+
+   <H5 class="title q-ma-sm text-center">{{ frases[index] }}</H5>
+
+   <div class="flex justify-around full-width">
+    <q-btn 
+      class="button"
+      @click="randomPosition" 
+      rounded 
+      label="skip"
+    />   
+    <q-btn 
+      class="button"
+      @click="randomPosition" 
+      rounded 
+      label="Gerar"
+    />   
+   </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'FraseDoDia',
+  data() {
+    return  { frases: [
+      'Frase 1',
+      'Frase 2',
+      'Frase 3',
+      'Frase 4',
+      'Frase 5'
+    ],
+    index: 0,
+   };
+  },
+   methods: {
+    randomPosition(){
+      let index = parseInt(Math.random() * 5 + 0);
+      this.index = index;
+   },
+  },
+  created(){
+    this.randomPosition();
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container { 
+    background-color: #f95179;
+  }
+  .title {
+    color: #fff;
+  }
+  .button {
+    color: #fff;
+    height: 60px;
+    background-color: #262525;
+  }
+
+</style>
